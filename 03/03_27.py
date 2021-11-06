169452f75e43891862fe2a2a0f81d947737b9b8a
Michal Miernik <s223304@student.uek.krakow.pl>
2021-11-06 21:24:30 +0100

    Zmieniono 03_27

--- 03/03_27.py
+++ 03/03_27.py
@@ -3,4 +3,12 @@ for i in range(6,-1,-3):
         print(f' {i+j}',end='')
     print()
 
+i=6
+while i > -1:
+    j=1
+    while j<4:
+        print(f' {i+j}',end='')
+        j=j+1
+    print()
+    i=i-3
 
