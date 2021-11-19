d55adad4f8401b49a4d3881789e09be065e86b3d
MichalMiernik223304 <92749157+MichalMiernik223304@users.noreply.github.com>
2021-11-19 20:54:32 +0100

    Add files via upload

---
+++ 08/08-9.py
@@ -0,0 +1,12 @@
+phone = {
+    "brand": "Apple",
+    "number": "78798743",
+    "connected": True,
+    "ram": "8",
+    "year": "2017",
+    "imei": "34235346"
+    }
+    
+for key, value in phone.items():
+    print(f"{key} = {value}")
+
